5337382b251b343c707a63559c8e144d63199eda
AndreaTurco <turcoandrea85@gmail.com>
2014-09-12 12:17:38 +0200

    mancano i numeri nell'ordine corretto

--- js/script.js
+++ js/script.js
@@ -8,19 +8,6 @@ $(document).ready(function(){
 	$.Operation( "cancOper" ).subscribe( $M.resetAll );
 	$.Operation( "doOperation" ).subscribe( $M.doOperationShowResult );
 
-
-//	$('#doOperation').on('click', function(){
-//		var operValue = {
-//            firstMember : parseFloat($('#firstMember').val()),
-//            secondMember : parseFloat($('#secondMember').val())
-//            },
-//            operToDo = $('#operToDo').val();
-//
-//		// Publisher
-//		$.Operation( operToDo ).publish( operValue );
-//	});
-
-
 	$('.button').on('click', function(){
         $M.updateValueToShow(this);
 	});
